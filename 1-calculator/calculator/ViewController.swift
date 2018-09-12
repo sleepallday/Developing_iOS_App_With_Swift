8831e1067050312c16b6a8fd049ec5db507805c1
sleepallday <804954084@qq.com>
2018-09-12 18:13:02 +0800

    initial UI

---
+++ 1-calculator/calculator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Li, Bo on 2018/9/6.
+//  Copyright © 2018 Li, Bo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        var digit = sender.currentTitle
+    }
+}
+
